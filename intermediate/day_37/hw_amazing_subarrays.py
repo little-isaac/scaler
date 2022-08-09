29674873d3a7cd9a2add9efbf0e0455d7b5feb99
Milind <milind@getflits.com>
2022-08-09 18:48:07 +0530

    =added solution for the amazing subarrays problem

---
+++ intermediate/day_37/hw_amazing_subarrays.py
@@ -0,0 +1,86 @@
+"""
+You are given a string S, and you have to find all the amazing substrings of S.
+
+An amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+
+Input
+
+Only argument given is string S.
+Output
+
+Return a single integer X mod 10003, here X is the number of Amazing Substrings in given the string.
+Constraints
+
+1 <= length(S) <= 1e6
+S can have special characters
+Example
+
+Input
+    ABEC
+
+Output
+    6
+
+Explanation
+    Amazing substrings of given string are :
+    1. A
+    2. AB
+    3. ABE
+    4. ABEC
+    5. E
+    6. EC
+    here number of substrings are 6 and 6 % 10003 = 6.
+"""
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def appOne(self,A):
+        startsWith = [ord('a'),
+                    ord('e'),
+                    ord('i'),
+                    ord('o'),
+                    ord('u'),
+                    ord('A'),
+                    ord('E'),
+                    ord('I'),
+                    ord('O'),
+                    ord('U')]
+        ans = 0
+        s = 0
+        char = 0
+        A = list(A)
+        n = len(A)
+        for i in range(n-1,-1,-1):
+            ele = A[i]
+            aci = ord(ele)
+            if aci in startsWith:
+                s += 1
+                ans = ans + char + 1
+                char += 1
+            else:
+                char += 1
+            # print(i,A[i],s,char,ans)
+        # print(s,char)
+        return ans % 10003
+
+    def appTwo(self,A):
+        se = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        ans = 0
+        for i in range(len(A)):
+            if(A[i] in se):
+                ans += len(A) - i
+        return int(ans%10003)
+    def solve(self, A):
+        return self.appTwo(A)
+        return self.appOne(A)
+
+
+solu = Solution()
+array = [
+    ["ABEC"] , # 6
+    ["AzZGBauYuTknYjjWEEbLvqMQwnoSgXKBdHKEVpeoxYNNtBfrxQrPauttMzjKFayKwMeuChjzCocgAHfAmPCatOqarzLuabyTnxoheeocVshfERNssVPfRyPmwvOVGbzNAuvryYNWwIeyZLMlLbkcFFJRHjEIgIwOThRJJFpLbunVrbhAYsMtdsKslLAGElwrZjvZweIytMpPEYVmktQeNleNROcTjrNxXeHvOMMTMfqZHTUjetojHFzZwOekfAILYISANxeJFRNgeZDKoOTddXqxduPIjGXsRSSkgIqKMeSqlQwAKtdrkvHLgmKleNAPEztGMPmZzUuCImLAhzUnXmsVzFvJUTeIKleuRnMPNAPWJYAZLXgzTBPmkXVShbBSlIAJSeFgvRFvcoqfVFgHUefxUxuYFCfUxbxlOslUhYRFXKmMmqDIQhhfXyGqUwGMSYeLLpEsKAhvFUzavDCOUgtmmNMnsvfmCdPWuWIjuUfZCBTrWnaDopbqXcjzSqRMpQWIBNnMcOQZjDkjPkxsuwENYQyjgSHFJrgSLnwbEInBfdeIfBbVuZZbBrblJgKHOmLZACLQkSRxxQJeUMPIQutraxFtrRcSeqAejOTSqaFGglQOoWPkcNOnLIgfclWNtjGQRVMlqCPUnUlOLbHfkzUyNAmTsswWtZjIGUBrLmRmstHgVcRUgWdQTCEPRzjPVTMJRJocYHftwoRzOSyQexjmceRHdqFdgNuGmGTUdXQaNwKmvOUzZPzGCBVcbVLgMoQrESbpVGteVVntOwEWxXsZnSAoIfBSsWVhDFBuDTkcrnsPdmmSHymouxHlcgtjgKUAPznxsIRUjDFsrjadJjEtPaWTVBHpatqYeSgrpWJDOGfgIGQPcTIXVsCVyCfKMpcXWGkvwuRuTmvCbNMLeUkZrEpYZdlKAgFELfwCCbZCYBcXhfUrsIHfdwhYyxHKAMYMERwlyRtxObDoxBhjXmynYkmkYZrkzlCuvrhW"] , # 2099
+]
+for A in array:
+    ans = solu.solve(A[0])
+    print("output for ",A," is ",ans)
