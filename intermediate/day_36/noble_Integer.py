5c13c7e46f17541d1ff71a9c9f1448b65fa65cfd
Milind <milind@getflits.com>
2022-09-08 11:30:00 +0530

    =added solution for the noble integer problem

---
+++ intermediate/day_36/noble_Integer.py
@@ -0,0 +1,79 @@
+"""
+Problem Description
+Given an integer array A, find if an integer p exists in the array such that the number of integers greater than p in the array equals p.
+
+
+
+Problem Constraints
+1 <= |A| <= 2*105
+1 <= A[i] <= 107
+
+
+Input Format
+First and only argument is an integer array A.
+
+
+
+Output Format
+Return 1 if any such integer p is present else, return -1.
+
+
+
+Example Input
+Input 1:
+
+ A = [3, 2, 1, 3]
+Input 2:
+
+ A = [1, 1, 3, 3]
+
+
+Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ -1
+
+
+Example Explanation
+Explanation 1:
+
+ For integer 2, there are 2 greater elements in the array..
+Explanation 2:
+
+ There exist no integer satisfying the required conditions.
+"""
+
+class Solution:
+	# @param A : list of integers
+	# @return an integer
+    def solve(self, A):
+        n = len(A)
+        A.sort(reverse=True)
+        ans = 0
+        c = 0
+        if A[0] == 0:
+            ans = 1
+        for i in range(1, n):
+            if A[i] != A[i-1]:
+                c = i
+            if A[i] == c:
+                ans += 1
+        if ans > 0:
+            return 1
+        return -1
+
+
+
+solu = Solution()
+array = [
+    [[3, 2, 1, 3]] , # 1
+    [[1, 1, 3, 3]] , # -1
+    [[1,1,1,1]] , # -1
+    [[ -4, 7, 5, 3, 5, -4, 2, -1, -9, -8, -3, 0, 9, -7, -4, -10, -4, 2, 6, 1, -2, -3, -1, -8, 0, -8, -7, -3, 5, -1, -8, -8, 8, -1, -3, 3, 6, 1, -8, -1, 3, -9, 9, -6, 7, 8, -6, 5, 0, 3, -4, 1, -10, 6, 3, -8, 0, 6, -9, -5, -5, -6, -3, 6, -5, -4, -1, 3, 7, -6, 5, -8, -5, 4, -3, 4, -6, -7, 0, -3, -2, 6, 8, -2, -6, -7, 1, 4, 9, 2, -10, 6, -2, 9, 2, -4, -4, 4, 9, 5, 0, 4, 8, -3, -9, 7, -8, 7, 2, 2, 6, -9, -10, -4, -9, -5, -1, -6, 9, -10, -1, 1, 7, 7, 1, -9, 5, -1, -3, -3, 6, 7, 3, -4, -5, -4, -7, 9, -6, -2, 1, 2, -1, -7, 9, 0, -2, -2, 5, -10, -1, 6, -7, 8, -5, -4, 1, -9, 5, 9, -2, -6, -2, -9, 0, 3, -10, 4, -6, -6, 4, -3, 6, -7, 1, -3, -5, 9, 6, 2, 1, 7, -2, 5 ]]
+]
+for A in array:
+    ans = solu.solve(A[0])
+    print("output for ",A," is ",ans)
