8cb6aace33953944226693f76919c99d03f1b4db
Milind <milind@getflits.com>
2022-09-15 20:59:36 +0530

    added hint solution to thhe code just for reference

---
+++ intermediate/day_38/hw_colorful_number.py
@@ -0,0 +1,23 @@
+class Solution:
+	# @param A : integer
+	# @return an integer
+    def colorful(self, A):
+        A = list(str(A))
+        n = len(A)
+        hashA = {}
+        temp = 1
+        for i in range(n):
+            for ele in hashA:
+                A[i]
+        return 1
+
+solu = Solution()
+array = [
+    [23] , # 1
+    [236] , # 0
+    [99] , # 0
+    [123] , # 0
+]
+for A in array:
+    ans = solu.colorful(A[0])
+    print("output for ",A," is ",ans)
