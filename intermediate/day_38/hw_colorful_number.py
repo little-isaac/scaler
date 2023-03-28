9e72611e38f75c3422b7a63427e94e6c4621564e
Milind <milind@getflits.com>
2023-03-28 22:27:31 +0530

    Added solution for the colorful number

--- intermediate/day_38/hw_colorful_number.py
+++ intermediate/day_38/hw_colorful_number.py
@@ -1,14 +1,25 @@
 class Solution:
 	# @param A : integer
 	# @return an integer
-    def colorful(self, A):
-        A = list(str(A))
-        n = len(A)
-        hashA = {}
-        temp = 1
-        for i in range(n):
-            for ele in hashA:
-                A[i]
+    def colorful(self, n):
+        # Convert input number to string
+        s = str(n)
+        # Initialize hash set to store products of consecutive subsequences
+        products = set()
+        # Loop over all possible subsequence lengths
+        for length in range(1, len(s)+1):
+            # Loop over all possible starting positions for current subsequence length
+            for i in range(len(s)-length+1):
+                # Calculate product of current subsequence
+                product = 1
+                for j in range(i, i+length):
+                    product *= int(s[j])
+                # If product is already in hash set, input number is not colorful
+                if product in products:
+                    return 0
+                # Otherwise, add product to hash set
+                products.add(product)
+        # If we processed all possible subsequences without finding a duplicate product, input number is colorful
         return 1
 
 solu = Solution()
