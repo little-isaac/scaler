76dab21ebd80bd7b016598bf981dde58530dfc1e
Milind Patel <milind@getflits.com>
2023-06-01 21:06:48 +0530

    template changed

--- Template/q1.py
+++ Template/q1.py
@@ -4,8 +4,16 @@
 """
 class Solution:
     def solve(self):
+        pass
 
-
+if __name__ == "__main__":
+    solu = Solution()
+    array = [
+        [] ,
+    ]
+    for A in array:
+        ans = solu.solve(A[0],A[1])
+        print("output for ",A," is ",ans)
 # solu = Solution()
 # array = [
 #     [] , 
